3779d1d83ecfd1c9338fadbb31050a310be5126d
Akasaka Yosuke <yskaksk@gmail.com>
2022-04-13 02:26:43 +0900

    とりあえずコミット　

--- src/compute.wgsl
+++ src/compute.wgsl
@@ -1,18 +1,142 @@
 struct Particle {
   pos : vec2<f32>,
   vel : vec2<f32>,
+  acc : vec2<f32>,
 };
+
 struct SimParams {
-  deltaT : f32,
-  beta : f32,
+    q_charge: f32,
+    blank_level: f32,
+    time_delta: f32,
+    d_max: f32,
+    sustain: f32,
+    dt: f32,
+    dt_2: f32,
+    dtt: f32,
+    v_max: f32,
+    scale: f32
 };
 
 @group(0) @binding(0) var<uniform> params : SimParams;
-@group(0) @binding(1) var<storage, read> particleSrc : array<Particle>;
-@group(0) @binding(2) var<storage, read_write> particleDst : array<Particle>;
-@group(0) @binding(3) var<uniform> image : array<array<f32,128>,128>;
+@group(0) @binding(1) var<storage, read> particlesSrc : array<Particle>;
+@group(0) @binding(2) var<storage, read_write> particlesDst : array<Particle>;
+
+@group(1) @binding(0) var t: texture_2d<f32>;
+
+fn rotate(v: f32) -> f32 {
+    if (v > 1.0) {
+        return v - 2.0;
+    } else if (v < -1.0) {
+        return v + 2.0;
+    } else {
+        return v;
+    }
+}
 
 @stage(compute)
 @workgroup_size(64)
 fn main(@builtin(global_invocation_id) global_invocation_id: vec3<u32>) {
+  let total = arrayLength(&particlesSrc);
+  let index = global_invocation_id.x;
+  if (index >= total) {
+    return;
+  }
+
+  var vPos : vec2<f32> = particlesSrc[index].pos;
+  var vVel : vec2<f32> = particlesSrc[index].vel;
+  var vAcc : vec2<f32> = particlesSrc[index].acc;
+
+  let Epsilon = 0.0000001;
+  var acc : vec2<f32> = vec2<f32>(0.0, 0.0);
+
+  let texture_size = textureDimensions(t);
+  let texture_w = texture_size[0];
+  let texture_h = texture_size[1];
+
+  var ix : i32 = 0;
+  loop {
+    if (ix >= texture_w) {
+      break;
+    }
+    var iy : i32 = 0;
+    loop {
+      if (iy >= texture_h) {
+        break;
+      }
+
+      let texel = textureLoad(t, vec2<i32>(ix, iy), 0);
+      let bmpQ = (params.blank_level - (texel[0] * 0.299 + texel[1] * 0.587 + texel[2] * 0.114)) * params.scale;
+
+      let texpos = vec2<f32>(
+        2.0 * f32(ix) / f32(texture_w) - 1.0,
+        1.0 - 2.0 * f32(iy) / f32(texture_h)
+      );
+
+      var dp : vec2<f32> = texpos - vPos;
+      dp[0] = rotate(dp[0]);
+      dp[1] = rotate(dp[1]);
+      let d2 = dot(dp, dp) + 0.00003;
+      let d = sqrt(d2);
+
+      if (abs(d) > Epsilon) {
+          let q = bmpQ / d2;
+          acc += q * dp / d;
+      }
+
+      continuing {
+        iy = iy + 1;
+      }
+    }
+    continuing {
+      ix = ix + 1;
+    }
+  }
+
+  var i : u32 = 0u;
+  loop {
+    if (i >= total) {
+      break;
+    }
+    if (i == index) {
+      continue;
+    }
+
+    let dotPos = particlesSrc[i].pos;
+    var dp : vec2<f32> = dotPos - vPos;
+    dp.x = rotate(dp.x);
+    dp.y = rotate(dp.y);
+
+    let d2 = dot(dp, dp) + 0.00003;
+    let d = sqrt(d2);
+    if (abs(d) > Epsilon) {
+      let q = params.q_charge / d2;
+      acc -= q * dp / d;
+    }
+
+    continuing {
+      i = i + 1u;
+    }
+  }
+
+  acc *= params.q_charge;
+
+  var vel : vec2<f32> = (params.sustain * vVel) + params.dt_2 * (vAcc + acc);
+  var vel : vec2<f32> = vVel;
+  let vlen = length(vel);
+  if (vlen > params.v_max) {
+      vel *= params.v_max / vlen;
+  }
+
+  var dp : vec2<f32> = (vel * params.dt) + (0.5 * acc * params.dtt);
+  let dlen = length(dp);
+  if (dlen > params.d_max) {
+      dp *= params.d_max / dlen;
+  }
+  var pos : vec2<f32> = vPos + dp;
+
+  pos.x = rotate(pos.x);
+  pos.y = rotate(pos.y);
+
+  // Write back
+  particlesDst[index] = Particle(pos, vel, acc);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,33 @@
 use std::{borrow::Cow, mem};
-use std::path::Path;
 use rand::{distributions::{Distribution, Uniform}, SeedableRng};
-use ndarray::Array2;
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
-    window::{Window, WindowBuilder}
+    window::{Window, WindowBuilder},
+    dpi::PhysicalSize
 };
 use wgpu::util::DeviceExt;
-use image::{self, GenericImageView, imageops::FilterType};
+use image::{self, GenericImageView};
 
-
-const NUM_PARTICLES: u32 = 1500;
 const PARTICLES_PER_GROUP: u32 = 64;
 
-const BLANK_LEVEL: f32 = 0.95;
+// namiura_high_square
+//const NUM_PARTICLES: u32 = 32768;
+//const Q_CHARGE: f32 = 0.4;
+//const BLANK_LEVEL: f32 = 0.95;
+//const TIME_DELTA: f32 = 0.001;
+//const D_MAX: f32 = 0.005;
+//const SUSTAIN: f32 = 0.9;
+//const SCALER: f32 = 2.0;
 
-const SIZE_W: u32 = 128;
-const SIZE_H: u32 = 128;
-
-fn load_image_as_mat(img_path: &Path) -> [[f32; SIZE_H as usize]; SIZE_W as usize] {
-    let img = image::open(img_path).unwrap();
-    let mut mat = [[0.0 as f32; SIZE_H as usize]; SIZE_W as usize];
-    for (x, y, p) in img.resize(SIZE_W, SIZE_H, FilterType::Triangle).to_rgb8().enumerate_pixels() {
-        mat[x as usize][y as usize] = BLANK_LEVEL - (0.2989 * p[0] as f32 + 0.587 * p[1] as f32 + 0.114 * p[2] as f32) / 255.9;
-    }
-    return mat
-}
+// black cat
+const NUM_PARTICLES: u32 = 32768;
+const Q_CHARGE: f32 = 0.4;
+const BLANK_LEVEL: f32 = 0.9;
+const TIME_DELTA: f32 = 0.001;
+const D_MAX: f32 = 0.005;
+const SUSTAIN: f32 = 0.9;
+const SCALER: f32 = 1.5;
 
 struct Model {
     surface: wgpu::Surface,
@@ -35,6 +36,7 @@ struct Model {
     particle_bind_groups: Vec<wgpu::BindGroup>,
     particle_buffers: Vec<wgpu::Buffer>,
     vertices_buffer: wgpu::Buffer,
+    texture_bind_group: wgpu::BindGroup,
     compute_pipeline: wgpu::ComputePipeline,
     render_pipeline: wgpu::RenderPipeline,
     work_group_count: u32,
@@ -76,21 +78,100 @@ impl Model {
         };
         surface.configure(&device, &config);
 
+        //let img_bytes = include_bytes!("../assets/cat_white_square_400px.png");
+        //let img_bytes = include_bytes!("../assets/namiura_high_square.png");
+        //let img_bytes = include_bytes!("../assets/cat_white_square.png");
+        //let img_bytes = include_bytes!("../assets/cat_face.png");
+        let img_bytes = include_bytes!("../assets/cat_3_square.png");
+        let img = image::load_from_memory(img_bytes).unwrap();
+        let img_rgba = img.to_rgba8();
+
+        let (img_w, img_h) = img.dimensions();
+        let mut total_charge = 0.0;
+        for (_, _, p) in img_rgba.enumerate_pixels() {
+            total_charge += BLANK_LEVEL - (0.299 * p[0] as f32 + 0.578 * p[1] as f32 + 0.11 * p[2] as f32) / 255.0;
+        }
+        let dot_charge_total = NUM_PARTICLES as f32 * Q_CHARGE;
+
+        let texture_size = wgpu::Extent3d {
+            width: img_w,
+            height: img_h,
+            depth_or_array_layers: 1,
+        };
+        let texture = device.create_texture(
+            &wgpu::TextureDescriptor {
+                size: texture_size,
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+                label: Some("texture")
+            }
+        );
+        queue.write_texture(
+            wgpu::ImageCopyTexture {
+                texture: &texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+                aspect: wgpu::TextureAspect::All,
+            },
+            &img_rgba,
+            wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: std::num::NonZeroU32::new(4 * img_w),
+                rows_per_image: std::num::NonZeroU32::new(img_h)
+            },
+            texture_size,
+        );
+        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let texture_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::Texture {
+                        multisampled: false,
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                        sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                    },
+                    count: None,
+                },
+            ],
+            label: Some("texture_bind_group_layout"),
+        });
+        let texture_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &texture_bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&view),
+                },
+            ],
+            label: Some("diffuse_bind_group"),
+        });
+
+
+        let dt = TIME_DELTA;
+        let dt_2 = 0.5 * dt;
+        let dtt = dt * dt;
+        let v_max = D_MAX / dt;
+        let scale = SCALER * dot_charge_total / total_charge;
         let param_data : Vec<f32> = vec![
-            0.1,
-            0.2
+            Q_CHARGE,
+            BLANK_LEVEL,
+            TIME_DELTA,
+            D_MAX,
+            SUSTAIN,
+            dt,
+            dt_2,
+            dtt,
+            v_max,
+            scale
         ];
         let param_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Parameter Buffer"),
             contents: bytemuck::cast_slice(&param_data),
-            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
-        });
-
-        let img_path = Path::new("./assets/cat_white.jpg");
-        let img_mat = load_image_as_mat(img_path);
-        let img_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Image Buffer"),
-            contents: bytemuck::cast_slice(&img_mat),
             usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
         });
 
@@ -128,23 +209,13 @@ impl Model {
                     },
                     count: None,
                 },
-                wgpu::BindGroupLayoutEntry {
-                    binding: 3,
-                    visibility: wgpu::ShaderStages::COMPUTE,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Uniform,
-                        has_dynamic_offset: false,
-                        min_binding_size: None,
-                    },
-                    count: None
-                }
             ],
             label: None,
         });
 
         let compute_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
             label: Some("compute"),
-            bind_group_layouts: &[&compute_bind_group_layout],
+            bind_group_layouts: &[&compute_bind_group_layout, &texture_bind_group_layout],
             push_constant_ranges: &[],
         });
         let compute_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
@@ -190,22 +261,27 @@ impl Model {
             multiview: None,
         });
 
-        //let vertex_buffer_data = [-0.005f32, -0.01, 0.005, -0.01, 0.00, 0.01];
-        let vertex_buffer_data = [-0.005f32, -0.005, 0.005, 0.005, -0.005, 0.005, -0.005, -0.005, 0.005, -0.005, 0.005, 0.005];
+        let vertex_buffer_data = [
+            0.005f32, 0.00,
+            -0.0025, 0.004,
+            -0.0025, -0.004,
+        ];
         let vertices_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Vertex Buffer"),
             contents: bytemuck::bytes_of(&vertex_buffer_data),
             usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
         });
 
-        let mut initial_particle_data : Vec<f32> = vec![0.0; (4 * NUM_PARTICLES) as usize];
-        let mut rng = rand::rngs::StdRng::seed_from_u64(13);
+        let mut initial_particle_data : Vec<f32> = vec![0.0; (6 * NUM_PARTICLES) as usize];
+        let mut rng = rand::rngs::StdRng::seed_from_u64(333);
         let unif = Uniform::new_inclusive(-1.0, 1.0);
-        for particle_instance_chunk in initial_particle_data.chunks_mut(4) {
+        for particle_instance_chunk in initial_particle_data.chunks_mut(6) {
             particle_instance_chunk[0] = unif.sample(&mut rng);
             particle_instance_chunk[1] = unif.sample(&mut rng);
-            particle_instance_chunk[2] = unif.sample(&mut rng) * 0.1;
-            particle_instance_chunk[3] = unif.sample(&mut rng) * 0.1;
+            particle_instance_chunk[2] = 0.0;
+            particle_instance_chunk[3] = 0.0;
+            particle_instance_chunk[4] = 0.0;
+            particle_instance_chunk[5] = 0.0;
         }
 
         let mut particle_buffers = Vec::<wgpu::Buffer>::new();
@@ -236,10 +312,6 @@ impl Model {
                         binding: 2,
                         resource: particle_buffers[(i+1)%2].as_entire_binding(),
                     },
-                    wgpu::BindGroupEntry {
-                        binding: 3,
-                        resource: img_buffer.as_entire_binding(),
-                    }
                 ],
                 label: None
             }));
@@ -254,6 +326,7 @@ impl Model {
             particle_bind_groups,
             particle_buffers,
             vertices_buffer,
+            texture_bind_group,
             compute_pipeline,
             render_pipeline,
             work_group_count,
@@ -262,13 +335,15 @@ impl Model {
 
     }
 
-    fn input(&mut self, _: &WindowEvent) -> bool { false }
+    fn input(&mut self, _: &WindowEvent) -> bool {
+        false
+    }
 
     fn update(&mut self) {}
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        let output = self.surface.get_current_texture()?;
-        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let frame = self.surface.get_current_texture()?;
+        let view = frame.texture.create_view(&wgpu::TextureViewDescriptor::default());
 
         let color_attachments = [wgpu::RenderPassColorAttachment {
             view: &view,
@@ -293,7 +368,8 @@ impl Model {
             let mut compute_pass = command_encoder.begin_compute_pass(&wgpu::ComputePassDescriptor { label: None });
             compute_pass.set_pipeline(&self.compute_pipeline);
             compute_pass.set_bind_group(0, &self.particle_bind_groups[self.frame_num % 2], &[]);
-            compute_pass.dispatch(self.work_group_count, 1, 1, );
+            compute_pass.set_bind_group(1, &self.texture_bind_group, &[]);
+            compute_pass.dispatch(self.work_group_count, 1, 1);
         }
 
         {
@@ -306,8 +382,8 @@ impl Model {
         self.frame_num += 1;
 
         self.queue.submit(Some(command_encoder.finish()));
-        output.present();
 
+        frame.present();
         Ok(())
     }
 }
@@ -315,7 +391,9 @@ impl Model {
 fn main() {
     env_logger::init();
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let window = WindowBuilder::new()
+        .with_inner_size(PhysicalSize::new(1800.0, 1800.0))
+        .build(&event_loop).unwrap();
 
     let mut model: Model = pollster::block_on(Model::new(&window));
 
